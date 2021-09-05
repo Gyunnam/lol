0fc22fc7f0cbbeaad58b5b94bc4cb8a6dc37a21e
Gyunnam <gyuni9604@naver.com>
2021-09-05 20:36:20 +0900

    .

---
+++ src/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+    margin: 0;
+    padding: 0;  
+    }
+
+    body {
+        box-sizing: border - box;
+        font-size : 12px;
+        line-height : 1em
+        color : color: #333;
+    }
+    
+    ul {
+        list-style: none;
+    }
+`
+
+export default GlobalStyle
---
+++ src/components/pages/champion/champion.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import ChampionData from 'http://ddragon.leagueoflegends.com/cdn/11.17.1/data/en_US/ Champion.json';
+
+function Champion(){
+    return (
+        <div>
+            
+        </div>
+    );
+}
+export default Champion;
